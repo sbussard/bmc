29315150c79b0f90836c8356ac8dae3cc2b5e42c
sbussard <me@stephenbussard.com>
2014-07-22 13:08:05 -0600

    added basic add/remove functionality

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,29 +4,42 @@ var bmcControllers = angular.module('bmcControllers', []);
 
 function addNew($scope) {
 	var func = function() {
-		$scope.list.push(prompt('Name:'));
+		var itemname = prompt('New item name');
+		if(itemname) {
+			$scope.list.push(itemname);
+		}
 	}
 	return func;
 }
 
-function makeController(name, icon) {
+function deleteItem($scope) {
+	var func = function($index) {
+		if(confirm('Delete ' + $scope.list[$index] + '?')) {
+			$scope.list.splice($index, 1);
+		}
+	}
+	return func;
+}
+
+function makeController(cname, name, icon) {
 	var controller = function($scope) {
 		$scope.name = name;
 		$scope.icon = icon;
 		$scope.list = [];
 		$scope.addNew = addNew($scope);
+		$scope.deleteItem = deleteItem($scope);
 	};
-	return controller;
+	bmcControllers.controller(cname, controller);
 }
 
 // controllers 
 
-bmcControllers.controller('KeyPartnersC', makeController("Key Partners", "link"));
-bmcControllers.controller('KeyActivitiesC', makeController("Key Activities", "cogs"));
-bmcControllers.controller('KeyResourcesC', makeController("Key Resources", "space-shuttle"));
-bmcControllers.controller('ValuePropositionsC', makeController("Value Propositions", "trophy"));
-bmcControllers.controller('CustomerRelationshipsC', makeController("Customer Relationships", "heart"));
-bmcControllers.controller('ChannelsC', makeController("Channels", "truck"));
-bmcControllers.controller('CustomerSegmentsC', makeController("Customer Segments", "group"));
-bmcControllers.controller('CostStructureC', makeController("Cost Structure", "tags"));
-bmcControllers.controller('RevenueStreamsC', makeController("Revenue Streams", "dollar"))
+makeController('KeyPartnersC', 'Key Partners', 'link');
+makeController('KeyActivitiesC', 'Key Activities', 'cogs');
+makeController('KeyResourcesC', 'Key Resources', 'space-shuttle');
+makeController('ValuePropositionsC', 'Value Propositions', 'trophy');
+makeController('CustomerRelationshipsC', 'Customer Relationships', 'heart');
+makeController('ChannelsC', 'Channels', 'truck');
+makeController('CustomerSegmentsC', 'Customer Segments', 'group');
+makeController('CostStructureC', 'Cost Structure', 'tags');
+makeController('RevenueStreamsC', 'Revenue Streams', 'dollar');
