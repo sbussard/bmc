47c22b0c0d0226a31971b3bee2612b4b1b66ac6f
sbussard <me@stephenbussard.com>
2014-07-22 14:39:39 -0600

    added descriptions and speech

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -21,25 +21,36 @@ function deleteItem($scope) {
 	return func;
 }
 
-function makeController(cname, name, icon) {
+function showDesc($scope) {
+	var func = function() {
+		var msg = new SpeechSynthesisUtterance($scope.desc);
+		window.speechSynthesis.speak(msg);
+	}
+
+	return func;
+}
+
+function makeController(block) {
 	var controller = function($scope) {
-		$scope.name = name;
-		$scope.icon = icon;
+		$scope.name = block.name;
+		$scope.icon = block.icon;
+		$scope.desc = block.desc;
 		$scope.list = [];
 		$scope.addNew = addNew($scope);
 		$scope.deleteItem = deleteItem($scope);
+		$scope.showDesc = showDesc($scope);
 	};
-	bmcControllers.controller(cname, controller);
+	bmcControllers.controller(block.cname, controller);
 }
 
 // controllers 
 
-makeController('KeyPartnersC', 'Key Partners', 'link');
-makeController('KeyActivitiesC', 'Key Activities', 'cogs');
-makeController('KeyResourcesC', 'Key Resources', 'space-shuttle');
-makeController('ValuePropositionsC', 'Value Propositions', 'trophy');
-makeController('CustomerRelationshipsC', 'Customer Relationships', 'heart');
-makeController('ChannelsC', 'Channels', 'truck');
-makeController('CustomerSegmentsC', 'Customer Segments', 'group');
-makeController('CostStructureC', 'Cost Structure', 'tags');
-makeController('RevenueStreamsC', 'Revenue Streams', 'dollar');
+makeController({ cname: 'KeyPartnershipsC', name: 'Key Partnerships', icon: 'link', desc: 'the network of suppliers and partners that make the business model work'});
+makeController({ cname: 'KeyActivitiesC', name: 'Key Activities', icon: 'cogs', desc: 'the most important things a company must do to make a business model work'});
+makeController({ cname: 'KeyResourcesC', name: 'Key Resources', icon: 'space-shuttle', desc: 'the most important asset required to make a business model work'});
+makeController({ cname: 'ValuePropositionsC', name: 'Value Propositions', icon: 'trophy', desc: 'bundle of products and services that cerate value for a customer segment'});
+makeController({ cname: 'CustomerRelationshipsC', name: 'Customer Relationships', icon: 'heart', desc: 'the types of relationships a company establishes with specific customer segments'});
+makeController({ cname: 'ChannelsC', name: 'Channels', icon: 'truck', desc: 'how a company communicates with and reaches its customer segments to deliver a value proposition'});
+makeController({ cname: 'CustomerSegmentsC', name: 'Customer Segments', icon: 'group', desc: 'defines different groups of people an organization aims to reach'});
+makeController({ cname: 'CostStructureC', name: 'Cost Structure', icon: 'tags', desc: 'describes all costs incurred to operate a business model'});
+makeController({ cname: 'RevenueStreamsC', name: 'Revenue Streams', icon: 'dollar', desc: 'the cash a company generates from each customer segment'});
