233f1709687a7611b3d1367adf5f8ba65de583bb
sbussard <me@stephenbussard.com>
2014-07-23 14:41:01 -0600

    it doesn't work but it should be committed somewhere

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,107 @@
-var bmcApp = angular.module('bmcApp', ['bmcControllers']);
+(function() {
+	var bmcApp = angular.module('bmcApp', ['bmcControllers']);
 
-bmcApp.run(function($rootScope) {
-		$rootScope.clear_canvas = function() {
-			$rootScope.canvas_data = {
-				'Key Partnerships': [],
-				'Key Activities': [],
-				'Key Resources': [],
-				'Value Propositions': [],
-				'Customer Relationships': [],
-				'Channels': [],
-				'Customer Segments': [],
-				'Cost Structure': [],
-				'Revenue Streams': []
+	bmcApp.run(function($rootScope) {
+			$rootScope.clear_canvas = function() {
+				$rootScope.canvas_data = {
+					'KeyPartnerships': [],
+					'KeyActivities': [],
+					'KeyResources': [],
+					'ValuePropositions': [],
+					'CustomerRelationships': [],
+					'Channels': [],
+					'CustomerSegments': [],
+					'CostStructure': [],
+					'RevenueStreams': []
+				};
+
+				$rootScope.update_canvas();
 			};
 
-			$rootScope.update_canvas();
-		};
+			$rootScope.observer_functions = [];
 
-		$rootScope.observer_functions = [];
+			$rootScope.update_canvas = function() {
+				console.log('update_canvas' + JSON.stringify($rootScope.observer_functions));
+				for(var i in $rootScope.observer_functions) {
+					$rootScope.observer_functions[i]();
+				}
+			};
 
-		$rootScope.update_canvas = function() {
-			for(var i in $rootScope.observer_functions) {
-				$rootScope.observer_functions[i]();
+			$rootScope.load_canvas_data = function(data) {
+				$rootScope.canvas_data = data;
+				$rootScope.update_canvas();
+			};
+
+			$rootScope.clear_canvas();
+	});
+
+	// directives 
+
+	// allows main element to load files
+	bmcApp.directive('main', function($rootScope) {
+		var data = {
+			restrict: 'E',
+			scope: {},
+			link: function($scope, element, attrs) {
+
+				function onDragEnter(e) {
+					e.stopPropagation();
+					e.preventDefault();
+				}
+
+				function onDragOver(e) {
+					e.stopPropagation();
+					e.preventDefault();
+				}
+
+				function onDragLeave(e) {
+					e.stopPropagation();
+					e.preventDefault();
+				}
+
+				function onDropFile(e) {
+					e.stopPropagation();
+					e.preventDefault();
+
+					// if(confirm('Would you like to load this canvas?\n\nyour current canvas will be gone forever if it\'s not saved!')) {
+						var file = e.dataTransfer.files[0];
+						var reader = new FileReader();
+
+						reader.readAsText(file);
+
+						reader.onload = function(event) {
+							content = JSON.parse(event.target.result);
+							$rootScope.load_canvas_data(content);
+						};
+					// }
+				}
+
+				element.bind('dragenter', onDragEnter);
+				element.bind('dragover', onDragOver);
+				element.bind('dragleave', onDragLeave);
+				element.bind('drop', onDropFile);
+
+				return element;
 			}
 		};
 
-		$rootScope.clear_canvas();
-});
+		return data;
+	});
+
+	bmcApp.directive('block', function() {
+		var data = {
+			restrict: 'E',
+			scope: {
+				block: '=info'
+			},
+			link: function($scope, element, attrs) {
+				element.addClass(attrs.info);
+			},
+			templateUrl: 'public/partials/block.html'
+		};
+		
+		return data;
+	});
+
+
+}());
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,87 +1,135 @@
-var bmcControllers = angular.module('bmcControllers', []);
+(function() {
+	var bmcControllers = angular.module('bmcControllers', []);
 
-// helpers
+	// helpers
 
-function addNew($scope, $rootScope) {
-	var func = function() {
-		var itemname = prompt('New item name');
-		if(itemname) {
-			$rootScope.canvas_data[$scope.name].push(itemname);
-			$scope.list = $rootScope.canvas_data[$scope.name];
-			console.log(JSON.stringify($rootScope.canvas_data));
+	function addNew($scope, $rootScope) {
+		var func = function() {
+			var itemname = prompt('New item name');
+			if(itemname) {
+				$rootScope.canvas_data[$scope.name].push(itemname);
+				$scope.list = $rootScope.canvas_data[$scope.name];
+				console.log(JSON.stringify($rootScope.canvas_data));
+			}
 		}
+		return func;
 	}
-	return func;
-}
 
-function deleteItem($scope, $rootScope) {
-	var func = function($index) {
-		if(confirm('Delete ' + $scope.list[$index] + '?')) {
-			$rootScope.canvas_data[$scope.name].splice($index, 1);
-			$scope.list = $rootScope.canvas_data[$scope.name];
-			console.log(JSON.stringify($rootScope.canvas_data));
+	function deleteItem($scope, $rootScope) {
+		var func = function($index) {
+			if(confirm('Delete ' + $scope.list[$index] + '?')) {
+				$rootScope.canvas_data[$scope.name].splice($index, 1);
+				$scope.list = $rootScope.canvas_data[$scope.name];
+				console.log(JSON.stringify($rootScope.canvas_data));
+			}
 		}
+		return func;
 	}
-	return func;
-}
 
-function showDesc($scope) {
-	var func = function() {
-		var msg = new SpeechSynthesisUtterance($scope.desc);
-		window.speechSynthesis.speak(msg);
+	function showDesc($scope) {
+		var func = function() {
+			var msg = new SpeechSynthesisUtterance($scope.desc);
+			window.speechSynthesis.speak(msg);
+		}
+		return func;
 	}
-	return func;
-}
 
-function makeController(block) {
-	var controller = function($scope, $rootScope) {
-		$scope.name = block.name;
-		$scope.icon = block.icon;
-		$scope.desc = block.desc;
-		$scope.list = [];
-		$scope.addNew = addNew($scope, $rootScope);
-		$scope.deleteItem = deleteItem($scope, $rootScope);
-		$scope.showDesc = showDesc($scope);
-		$scope.update = function() {
-			$scope.list = $rootScope.canvas_data[$scope.name];
+	// controllers 
+
+	bmcControllers.controller('IOController', function($scope, $rootScope) {
+		$scope.saveBMC = function() {
+			var canvas_data = JSON.stringify($rootScope.canvas_data);
+			var dlObj = document.createElement('a');
+		  dlObj.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(canvas_data));
+		  dlObj.setAttribute('download', 'untitled_canvas.json');
+		  dlObj.click();
+		}
+
+		$scope.loadBMC = function() {
+			alert('To open a canvas, drop a saved file from your desktop anywhere on the canvas');
+		}
+
+		$scope.clearBMC = function() {
+			if(confirm('Do you want to clear this canvas?')) {
+				$rootScope.clear_canvas();
+			}
+		}
+	});
+
+	bmcControllers.controller('CanvasController', function($scope, $rootScope) {
+		$scope.KeyPartnerships = { 
+			name: 'Key Partnerships', 
+			icon: 'link', 
+			section: 8,
+			items: [], 
+			desc: 'the network of suppliers and partners that make the business model work'
 		};
-		$rootScope.observer_functions.push($scope.update);
-	};
-	bmcControllers.controller(block.cname, controller);
-}
+		$scope.KeyActivities = { 
+			name: 'Key Activities', 
+			icon: 'cogs', 
+			section: 7,
+			items: [], 
+			desc: 'the most important things a company must do to make a business model work'
+		};
+		$scope.KeyResources = { 
+			name: 'Key Resources', 
+			icon: 'space-shuttle', 
+			section: 6,
+			items: [], 
+			desc: 'the most important asset required to make a business model work'
+		};
+		$scope.ValuePropositions = { 
+			name: 'Value Propositions', 
+			icon: 'trophy', 
+			section: 2,
+			items: [], 
+			desc: 'bundle of products and services that cerate value for a customer segment'
+		};
+		$scope.CustomerRelationships = { 
+			name: 'Customer Relationships', 
+			icon: 'heart', 
+			section: 4,
+			items: [], 
+			desc: 'the types of relationships a company establishes with specific customer segments'
+		};
+		$scope.Channels = { 
+			name: 'Channels', 
+			icon: 'truck', 
+			section: 3,
+			items: [], 
+			desc: 'how a company communicates with and reaches its customer segments to deliver a value proposition'
+		};
+		$scope.CustomerSegments = { 
+			name: 'Customer Segments', 
+			icon: 'group', 
+			section: 1,
+			items: [], 
+			desc: 'defines different groups of people an organization aims to reach'
+		};
+		$scope.CostStructure = { 
+			name: 'Cost Structure', 
+			icon: 'tags', 
+			section: 9,
+			items: [], 
+			desc: 'describes all costs incurred to operate a business model'
+		};
+		$scope.RevenueStreams = { 
+			name: 'Revenue Streams', 
+			icon: 'dollar', 
+			section: 5,
+			items: [], 
+			desc: 'the cash a company generates from each customer segment'
+		};
 
-// controllers 
+		// $scope.addNew = addNew($scope, $rootScope);
+		// $scope.deleteItem = deleteItem($scope, $rootScope);
+		// $scope.showDesc = showDesc($scope);
 
-bmcControllers.controller('ioC', function($scope, $rootScope) {
-	$scope.saveBMC = function() {
-		var canvas_data = JSON.stringify($rootScope.canvas_data);
-		var dlObj = document.createElement('a');
-	  dlObj.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(canvas_data));
-	  dlObj.setAttribute('download', 'untitled_canvas.json');
-	  dlObj.click();
-	}
+		// $scope.update = function() {
+		// 	$scope.list = $rootScope.canvas_data[$scope.name];
+		// };
 
-	$scope.loadBMC = function() {
-		if(confirm('Would you like to load a canvas?\n\nyour current canvas will be gone forever if it\'s not saved')) {
-			var data = {"Key Partnerships":["stephen this is the latest update"],"Key Activities":["ka","lol"],"Key Resources":["kr"],"Value Propositions":[],"Customer Relationships":[],"Channels":[],"Customer Segments":[],"Cost Structure":[],"Revenue Streams":["rs1"]};
-			$rootScope.canvas_data = data;
-			$rootScope.update_canvas();
-		}
-	}
+		// $rootScope.observer_functions.push($scope.update);
+	});
 
-	$scope.clearBMC = function() {
-		if(confirm('Do you want to clear this canvas?')) {
-			$rootScope.clear_canvas();
-		}
-	}
-});
-
-makeController({ cname: 'KeyPartnershipsC', name: 'Key Partnerships', icon: 'link', desc: 'the network of suppliers and partners that make the business model work'});
-makeController({ cname: 'KeyActivitiesC', name: 'Key Activities', icon: 'cogs', desc: 'the most important things a company must do to make a business model work'});
-makeController({ cname: 'KeyResourcesC', name: 'Key Resources', icon: 'space-shuttle', desc: 'the most important asset required to make a business model work'});
-makeController({ cname: 'ValuePropositionsC', name: 'Value Propositions', icon: 'trophy', desc: 'bundle of products and services that cerate value for a customer segment'});
-makeController({ cname: 'CustomerRelationshipsC', name: 'Customer Relationships', icon: 'heart', desc: 'the types of relationships a company establishes with specific customer segments'});
-makeController({ cname: 'ChannelsC', name: 'Channels', icon: 'truck', desc: 'how a company communicates with and reaches its customer segments to deliver a value proposition'});
-makeController({ cname: 'CustomerSegmentsC', name: 'Customer Segments', icon: 'group', desc: 'defines different groups of people an organization aims to reach'});
-makeController({ cname: 'CostStructureC', name: 'Cost Structure', icon: 'tags', desc: 'describes all costs incurred to operate a business model'});
-makeController({ cname: 'RevenueStreamsC', name: 'Revenue Streams', icon: 'dollar', desc: 'the cash a company generates from each customer segment'});
+}());
